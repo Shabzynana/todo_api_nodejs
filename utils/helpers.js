222eb0caed5ff8b2e231c30ddcd7d184bc9b9187
MAC <horlarmihleykan10@gmail.com>
2024-06-19 00:18:26 +0100

    modified password functions

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt');
 
+// login required
 function authMiddleware(req, res, next) {
     if (req.session.user) {
       next();
@@ -10,33 +11,33 @@ function authMiddleware(req, res, next) {
 }  
 
 // Number of salt rounds for hashing
-
 // Function to hash a password
-// async function hashPassword(password) {
+// function hashPassword(password) {
 //     const saltRounds = 10; 
-//     try {
-//         // const salt = await bcrypt.genSalt(saltRounds);
-//         const hash = await bcrypt.hash(password, saltRounds);
-//         return hash;
-//     } catch (error) {
-//         throw new Error('Error hashing password');
-//     }
+//     // const salt = bcrypt.genSalt(saltRounds);
+//     const hash = bcrypt.hash(password, saltRounds);
+//     return (hash)
 // }
+
 
 // // Function to compare a password with a hash
 // async function comparePassword(password, hash) {
 //     try {
-//         const match = await bcrypt.compare(password, hash);
-//         return match;
+//         // const match = await bcrypt.compare(password, hash);
+//         return await bcrypt.compare(password, hash);
 //     } catch (error) {
 //         throw new Error('Error comparing password');
 //     }
 // }
+
+
+// // Function to hash a password
 function hashPassword(password) {
     const salt = bcrypt.genSaltSync();
     return bcrypt.hashSync(password, salt);
 }
-  
+
+// // Function to compare a password with a hash  
 function comparePassword(raw, hash) {
     return bcrypt.compareSync(raw, hash);
 }  
